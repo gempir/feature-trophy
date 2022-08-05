4d64c393789aca868164f79ec3e73e82ca502b27
gempir <gempir.dev@gmail.com>
2022-08-05 22:20:08 +0200

    fix print svg

--- src/CHECK24.tsx
+++ src/CHECK24.tsx
@@ -3,6 +3,8 @@ export function CHECK24() {
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 155 38"
         xmlSpace="preserve"
+        width={189}
+        height={46}
     >
         <path
             fill="#FFF"
