215cb2602b9364cda46f0d1bb07d30f4a399d27d
gempir <gempir.dev@gmail.com>
2022-08-05 21:33:59 +0200

    basic layout with banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import { CHECK24 } from "./CHECK24";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export function App() {
+    return <div className="trophy">
+        <div className="main-image">
+            <img src="https://dummyimage.com/1096x604/bbb/fff" alt={"Feature"} />
+        </div>
+        <div className="banner">
+            <CHECK24 />
+        </div>
+    </div>;
 }
-
-export default App;
