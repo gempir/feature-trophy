939d0791df630568793fd45b0727a03302ae0394
gempir <gempir.dev@gmail.com>
2022-08-05 21:50:07 +0200

    font setup and marker effect

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,19 @@ import "./App.scss";
 import { CHECK24 } from "./CHECK24";
 
 export function App() {
-    return <div className="trophy">
-        <div className="main-image">
-            <img src="https://dummyimage.com/1096x604/bbb/fff" alt={"Feature"} />
-        </div>
-        <div className="banner">
-            <CHECK24 />
+    return <div className="container">
+        <div className="trophy">
+            <div className="main-image">
+                <img src="https://dummyimage.com/1096x604/bbb/fff" alt={"Feature"} />
+            </div>
+            <div className="title">
+                <div className="text">
+                    Product Creation Prio Tool
+                </div>
+            </div>
+            <div className="banner">
+                <CHECK24 />
+            </div>
         </div>
     </div>;
 }
