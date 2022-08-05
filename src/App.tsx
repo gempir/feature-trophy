8f880ff3c60d5f34f38656268f427363cdb863b8
gempir <gempir.dev@gmail.com>
2022-08-05 22:41:59 +0200

    build a back side of print

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ import { CHECK24 } from "./CHECK24";
 export function App() {
     const [img, setImg] = useState<string>("https://dummyimage.com/1096x604/bbb/fff");
     const [title, setTitle] = useState<string>("Product Creation Pool");
-
+    const [releaseDate, setReleaseDate] = useState<string>("2022-01-01");
+    const [description, setDescription] = useState<string>("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
 
     return <div className="page">
         <div className="container">
@@ -24,7 +25,6 @@ export function App() {
             </div>
         </div>
         <div className="config">
-            <input className="title-input" type={"text"} onChange={e => setTitle(e.target.value)} value={title} />
             <input className="upload" type={'file'} onChange={(e) => {
                 const files = e.target?.files;
                 if (!files || files?.length === 0) {
@@ -39,6 +39,23 @@ export function App() {
                     reader.readAsDataURL(file);
                 }
             }} />
+            <input className="title-input" type={"text"} onChange={e => setTitle(e.target.value)} value={title} />
+            <input className="release-date-input" type={"date"} onChange={e => setReleaseDate(e.target.value)} value={releaseDate} />
+            <textarea className="description-input" onChange={e => setDescription(e.target.value)} value={description} />
+            <button onClick={window.print}>Print</button>
+        </div>
+        <div className="container description-container">
+            <div className="description">
+                <div className="release">
+                    {releaseDate}
+                </div>
+                <div className="title">
+                    {title}
+                </div>
+                <div className="description">
+                    {description}
+                </div>
+            </div>
         </div>
     </div>;
 }
